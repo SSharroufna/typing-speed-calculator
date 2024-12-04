1d7f8ec668cbcbe514ba6f0899ba8a487d14cfc3
SarahSharroufna <sash1048@colorado.edu>
2024-12-03 19:09:13 -0700

    Add letter checking is working

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,32 +4,51 @@ import { WordList } from '../public/WordList.js'
 
 function App() {
   const [index, setIndex] = useState(0);
-  const [item, setItem] = useState(WordList[index]);
   const [inputValue, setInputValue] = useState('');
-
+  
   const handleKeyDown = (event) => {
     console.log('Key pressed:', event.key);
-    if (event.key === 'Enter') {
-      console.log('Entered value:', event.key);
+    let letter = 0;
+
+    while ( event.target.value[letter] == WordList[index][letter] && letter < WordList[index].length  ) {
+        console.log('Correct');
+        letter++;
+    } 
+    console.log(letter);
+    console.log(WordList[index].length);
+
+    if ( letter == WordList[index].length){
       setIndex(index + 1);
-      setItem(WordList[index]);
       setInputValue(''); 
+      letter = 0;
     } else {
-      console.log('Key pressed:', event.key);
+      setInputValue(event.target.value);
     }
+    
+
+   
+
+
+    // if (event.key === 'Enter') {
+    //   console.log('Entered value:', event.key);
+    //   setIndex(index + 1);
+    //   setItem(WordList[index]);
+    //   setInputValue(''); 
+    // } else {
+    //   console.log('Key pressed:', event.key);
+    // }
   }
 
   return (
     < >
       <div key ={ index } >
-      <li className="word-display">{ item }</li>
+      <li className="word-display">{ WordList[index] }</li>
       </div>
       <div>
       <input
         type="text"
         value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={handleKeyDown}
+        onChange={handleKeyDown}
       />
       </div>
     </>
