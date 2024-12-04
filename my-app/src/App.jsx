f5b34631d6d477192af96606efa7b988801e0910
Gia <gibo8481@colorado.edu>
2024-12-03 18:06:20 -0700

    updated word array

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,1 +1,18 @@
-import {useState, useEffect} from 'react'
+// import {useState, useEffect} from 'react'
+import React from 'react'
+import './App.css'
+import { WordList } from '../public/WordList.js'
+
+function App() {
+
+  return (
+    <div >
+        { WordList.map((item , index) => (
+            <div key ={ index } >
+            <li className="word-display">{ item }</li>
+            </div>
+        ))}
+    </div>
+  )
+}
+export default App
