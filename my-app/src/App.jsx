b024ecf8db5a18f6a766e43796f1e407705462ac
SarahSharroufna <sash1048@colorado.edu>
2024-12-03 18:32:44 -0700

    Add input box and keyboard event listener

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,18 +1,38 @@
-// import {useState, useEffect} from 'react'
-import React from 'react'
+import React, { useState } from 'react';
 import './App.css'
 import { WordList } from '../public/WordList.js'
 
 function App() {
+  const [index, setIndex] = useState(0);
+  const [item, setItem] = useState(WordList[index]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = (event) => {
+    console.log('Key pressed:', event.key);
+    if (event.key === 'Enter') {
+      console.log('Entered value:', event.key);
+      setIndex(index + 1);
+      setItem(WordList[index]);
+      setInputValue(''); 
+    } else {
+      console.log('Key pressed:', event.key);
+    }
+  }
 
   return (
-    <div >
-        { WordList.map((item , index) => (
-            <div key ={ index } >
-            <li className="word-display">{ item }</li>
-            </div>
-        ))}
-    </div>
+    < >
+      <div key ={ index } >
+      <li className="word-display">{ item }</li>
+      </div>
+      <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      </div>
+    </>
   )
 }
 export default App
